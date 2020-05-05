8b7818b9aaec89bcf57ff8d409922fdea71adf6c
Squzet-Kaledo <64698160+Squzet-Kaledo@users.noreply.github.com>
2020-05-05 13:21:37 +0300

    Изменения

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,11 +1,6 @@
-public class MyFirstProgram {
-  public static void main(String[] args) {
+public class MyFirstProgram{
 
-    System.out.println("Hello, world!");
-  }
+public static void main(String[] args) {
+System.out.println("Hello, world!");
 }
-
-
-
-
-
+}
